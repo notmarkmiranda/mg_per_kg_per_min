94e32400f94d1c9b97e6c61e692b470fcb1ed78f
Mark Miranda <markmiranda@MacPro-102.lan>
2017-05-30 20:17:32 -0600

    wip

--- mg per kg per min/ViewController.swift
+++ mg per kg per min/ViewController.swift
@@ -8,95 +8,185 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    // MARK: - View Did Load
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setNextAndPrevious(patientWeightField)
+class ViewController: UIViewController, UITextFieldDelegate {
+  
+  // MARK: - View Did Load
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    hideErrors()
+    setDelegates()
+    setNextAndPrevious()
+    addAccessoryView(patientWeightField)
+  }
+  
+  // MARK: - Properties
+  var activeTextField: UITextField?
+  enum BlanksError : Error {
+    case tooMany
+    case none
+  }
+  var solveForField: UITextField?
+  
+  
+  // MARK: - Functions
+  
+  func hideErrors() {
+    errorLabel.isHidden = true
+  }
+  
+  func showErrors(message: String) {
+    errorLabel.text = message
+    errorLabel.isHidden = false
+  }
+  
+  func setNextAndPrevious() {
+    patientWeightField.prevField = mgKgMinField
+    patientWeightField.nextField = doseOfDrugGramsField
+    doseOfDrugGramsField.prevField = patientWeightField
+    doseOfDrugGramsField.nextField = doseOfDrugMLField
+    doseOfDrugMLField.prevField = doseOfDrugGramsField
+    doseOfDrugMLField.nextField = rateOfMLHrField
+    rateOfMLHrField.prevField = doseOfDrugMLField
+    rateOfMLHrField.nextField = mgKgMinField
+    mgKgMinField.prevField = rateOfMLHrField
+    mgKgMinField.nextField = patientWeightField
+    
+    
+  }
+  
+  func setDelegates() {
+    self.patientWeightField.delegate = self
+    self.doseOfDrugGramsField.delegate = self
+    self.doseOfDrugMLField.delegate = self
+    self.rateOfMLHrField.delegate = self
+    self.mgKgMinField.delegate = self
+  }
+  
+  func addAccessoryView(_ currentField: UITextField) -> Void {
+    let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: UIScreen.main.nativeBounds.width, height: 44))
+    
+    let prevButton = UIBarButtonItem(title: "PREV", style: .plain, target: self, action: #selector(previousField))
+    let space = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: self, action: nil)
+    space.width = 20
+    let nextButton = UIBarButtonItem(title: "NEXT", style: .plain, target: self, action: #selector(nextField))
+    let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+    let calcButton = UIBarButtonItem(title: "CALC", style: .plain, target: self, action: #selector(calculate))
+    
+    toolbar.items = [prevButton, space, nextButton, flexibleSpace, calcButton]
+    
+    currentField.inputAccessoryView = toolbar
+    currentField.becomeFirstResponder()
+  }
+  
+  
+  func previousField() {
+    addAccessoryView((activeTextField?.prevField)!)
+  }
+  
+  func nextField() {
+    addAccessoryView((activeTextField?.nextField)!)
+  }
+  
+  func calculate() {
+    do {
+      try findBlanks()
+      updateModel()
+    } catch BlanksError.none {
+      showErrors(message: "You have to leave at least 1 field blank.")
+    } catch BlanksError.tooMany {
+      showErrors(message: "You can only leave 1 field blank.")
+    } catch {
+      showErrors(message: "I'm not even sure how you got here.")
+    }
+  }
+  
+  func updateModel() {
+    // (1 / kg) * (dose g / total mL) * (1000mg / 1g) * (rate mL / 1 hour) * (1 hour / 60 min)
+    //    let x = mgKgMinField
+    
+    calcModel.patientWeight = patientWeightField.text!
+    calcModel.doseOfDrugGrams = doseOfDrugGramsField.text!
+    calcModel.doseOfDrugML = doseOfDrugMLField.text!
+    calcModel.rateOfMLHr = rateOfMLHrField.text!
+    calcModel.mgKgMin = mgKgMinField.text!
+    
+    calc()
+    
+    
+    // 70KG - Weight
+    // 240 - rate of mL / hour
+    // 800 - total mL of Drug
+    // 40 - Dose of Drug
+    // 2.857 - mg / kg / min
+  }
+  
+  func calc() {
+    switch solveForField! {
+    case mgKgMinField:
+      mgKgMinField.text = calcModel.solveFormGKgMin
+    case patientWeightField:
+      patientWeightField.text = calcModel.solveForPatientWeight
+    default: break
+    }
+  }
+  
+  func findBlanks() throws {
+    var blanks = 0
+    for field in [patientWeightField, doseOfDrugMLField, doseOfDrugMLField, rateOfMLHrField, mgKgMinField] {
+      if field!.text!.characters.count == 0 {
+        solveForField = field
+        blanks += 1
+      }
     }
     
-    // MARK: - Properties
-    var activeTextField: UITextField?
-    
-    // MARK: - Functions
-    func setNextAndPrevious(_ currentField: UITextField) {
-        patientWeightField.prevField = mgKgMinField
-        patientWeightField.nextField = doseOfDrugGramsField
-        doseOfDrugGramsField.prevField = patientWeightField
-        doseOfDrugGramsField.nextField = doseOfDrugMLField
-        doseOfDrugMLField.prevField = doseOfDrugGramsField
-        doseOfDrugMLField.nextField = rateOfMLHrField
-        rateOfMLHrField.prevField = doseOfDrugMLField
-        rateOfMLHrField.nextField = mgKgMinField
-        mgKgMinField.prevField = rateOfMLHrField
-        mgKgMinField.nextField = patientWeightField
-        addAccessoryView(currentField)
+    guard blanks == 1 else {
+      if blanks == 0 {
+        throw BlanksError.none
+      } else {
+        throw BlanksError.tooMany
+      }
+      
     }
-    
-    func addAccessoryView(_ currentField: UITextField) -> Void {
-        let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: UIScreen.main.nativeBounds.width, height: 44))
-        
-        let prevButton = UIBarButtonItem(title: "PREV", style: .plain, target: self, action: #selector(previousField))
-        let space = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: self, action: nil)
-        space.width = 20
-        let nextButton = UIBarButtonItem(title: "NEXT", style: .plain, target: self, action: #selector(nextField))
-        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
-        let calcButton = UIBarButtonItem(title: "CALC", style: .plain, target: self, action: #selector(calculate))
-        
-        toolbar.items = [prevButton, space, nextButton, flexibleSpace, calcButton]
-        
-        activeTextField = currentField
-        currentField.inputAccessoryView = toolbar
-        currentField.becomeFirstResponder()
-    }
-    
-    func previousField() {
-        addAccessoryView((activeTextField?.prevField)!)
-    }
-    
-    func nextField() {
-        addAccessoryView((activeTextField?.nextField)!)
-    }
-    
-    func calculate() {
-        if let weight = patientWeightField.text {
-            calcModel.patientWeight = Double(weight)
-        }
-        print(calcModel.patientWeight!)
-    }
-    
-    // MARK: - Outlets
-    @IBOutlet weak var patientWeightField: UITextField!
-    @IBOutlet weak var kgLbSwitch: UISwitch!
-    @IBOutlet weak var doseOfDrugGramsField: UITextField!
-    @IBOutlet weak var doseOfDrugMLField: UITextField!
-    @IBOutlet weak var rateOfMLHrField: UITextField!
-    @IBOutlet weak var mgKgMinField: UITextField!
-    
-    
+  }
+  
+  func textFieldDidBeginEditing(_ textField: UITextField) {
+    hideErrors()
+    addAccessoryView(textField)
+    activeTextField = textField
+  }
+  
+  // MARK: - Outlets
+  @IBOutlet weak var patientWeightField: UITextField!
+  @IBOutlet weak var kgLbSwitch: UISwitch!
+  @IBOutlet weak var doseOfDrugGramsField: UITextField!
+  @IBOutlet weak var doseOfDrugMLField: UITextField!
+  @IBOutlet weak var rateOfMLHrField: UITextField!
+  @IBOutlet weak var mgKgMinField: UITextField!
+  @IBOutlet weak var errorLabel: UILabel!
 }
+
 
 private var kAssociationKeyNextField: UInt8 = 0
 private var kAssociationKeyPrevField: UInt8 = 0
 
 extension UITextField {
-    var nextField: UITextField? {
-        get {
-            return objc_getAssociatedObject(self, &kAssociationKeyNextField) as? UITextField
-        }
-        set(newField) {
-            objc_setAssociatedObject(self, &kAssociationKeyNextField, newField, .OBJC_ASSOCIATION_RETAIN)
-        }
+  // MARK: - UITextField extensions
+  var nextField: UITextField? {
+    get {
+      return objc_getAssociatedObject(self, &kAssociationKeyNextField) as? UITextField
     }
-    
-    var prevField: UITextField? {
-        get {
-            return objc_getAssociatedObject(self, &kAssociationKeyPrevField) as? UITextField
-        }
-        set(newField) {
-            objc_setAssociatedObject(self, &kAssociationKeyPrevField, newField, .OBJC_ASSOCIATION_RETAIN)
-        }
+    set(newField) {
+      objc_setAssociatedObject(self, &kAssociationKeyNextField, newField, .OBJC_ASSOCIATION_RETAIN)
     }
-
+  }
+  
+  var prevField: UITextField? {
+    get {
+      return objc_getAssociatedObject(self, &kAssociationKeyPrevField) as? UITextField
+    }
+    set(newField) {
+      objc_setAssociatedObject(self, &kAssociationKeyPrevField, newField, .OBJC_ASSOCIATION_RETAIN)
+    }
+  }
+  
 }
--- mg per kg per min/calcModel.swift
+++ mg per kg per min/calcModel.swift
@@ -10,10 +10,46 @@ import Foundation
 let calcModel = CalcModel()
 
 class CalcModel {
-    var patientWeight: Double?
-    var kGOrLb: Bool = true
-    var doseOfDrugGrams: Double?
-    var doseOfDrugML: Double?
-    var rateOfMLHr: Double?
-    var mgKgMin: Double?
+  var patientWeight = "0"
+  var kgOrLb: Bool = true
+  var doseOfDrugGrams = "0"
+  var doseOfDrugML = "0"
+  var rateOfMLHr = "0"
+  var mgKgMin = "0"
+  
+  var patientWeightAsDecimal: NSDecimalNumber {
+    return NSDecimalNumber(string: patientWeight)
+  }
+  
+  var doseOfDrugGramsAsDecimal: NSDecimalNumber {
+    return NSDecimalNumber(string: doseOfDrugGrams)
+  }
+  
+  var doseofDrugMLAsDecimal: NSDecimalNumber {
+    return NSDecimalNumber(string: doseOfDrugML)
+  }
+  
+  var rateOfMLHrAsDecimal: NSDecimalNumber {
+    return NSDecimalNumber(string: rateOfMLHr)
+  }
+  
+  var mgKgMinAsDecimal: NSDecimalNumber {
+    return NSDecimalNumber(string: mgKgMin)
+  }
+  
+  let formatter = NumberFormatter()
+  
+  var solveFormGKgMin: String {
+    formatter.numberStyle = .decimal
+    let numerator = ((doseOfDrugGramsAsDecimal as Decimal) * 1000 * (rateOfMLHrAsDecimal as Decimal))
+    let denominator = ((patientWeightAsDecimal as Decimal) * (doseofDrugMLAsDecimal as Decimal) * 60)
+    return formatter.string(from: numerator / denominator as NSNumber)!
+  }
+  
+  var solveForPatientWeight: String {
+    formatter.numberStyle = .decimal
+    let numerator = ((doseOfDrugGramsAsDecimal as Decimal) * 1000 * (rateOfMLHrAsDecimal as Decimal))
+    let denominator = ((mgKgMinAsDecimal as Decimal) * (doseofDrugMLAsDecimal as Decimal) * 60)
+    return formatter.string(from: numerator / denominator as NSNumber)!
+  }
 }
